45d3ac3cb4e6d492be49dbf1d8e6bae1d6b1da7f
mohomadnavab <143999298+mohomadnavab@users.noreply.github.com>
2026-04-09 15:41:09 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.UseUrls("http://0.0.0.0:80");
+  
 var app = builder.Build();
 
 app.MapGet("/", () => "Blue-Green Deployment Demo Working");
