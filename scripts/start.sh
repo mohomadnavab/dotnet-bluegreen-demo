32ba37f71acecd2dc5cb624f8fa78a2b2a397972
Mohomadnavab <mohomadnavab.nadaf@outlook.com>
2026-04-10 14:29:33 +0530

    fix scripts

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 set -e
 
-APP_DIR=/home/ubuntu/app
-
-cd $APP_DIR
+cd /home/ubuntu/app
 
 # stop old process
 pkill -f dotnet || true
 
-# start application
-export ASPNETCORE_URLS=http://0.0.0.0:80
-
-nohup dotnet out/dotnet-bluegreen-demo.dll > app.log 2>&1 &
+nohup dotnet dotnet-bluegreen-demo.dll > app.log 2>&1 &
 
 sleep 5
